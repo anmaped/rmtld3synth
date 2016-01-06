cf60bed53cf124e688651975ab091830096b1f2f
Andre Pedro <anmap@isep.ipp.pt>
2016-01-06 23:06:54 +0000

    Adds some Nuttx makefiles. Removes deprecated project files.

--- nuttx/module.mk
+++ nuttx/module.mk
@@ -1,5 +1,8 @@
-# Auto-generated file - do not edit
-MODULE_COMMAND = Monitor
-SRCS = Monitor.cpp main.cpp
+#
+# rtemlib module for NuttX OS
+# 
+
+MODULE_COMMAND = rtemlib
+SRCS = main.cpp
 MODULE_STACKSIZE = 4096
 EXTRACXXFLAGS = -Wframe-larger-than=1200 -DCONFIG_WCHAR_BUILTIN -I$(SKETCHBOOK)/../rteml/arch/arm/include -DARM_CM4_FP -D__NUTTX__ -std=c++0x --verbose
