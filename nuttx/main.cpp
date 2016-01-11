c04024fc4c274fc7d898ad9bea25233200b49d0a
Andre Pedro <anmap@isep.ipp.pt>
2016-01-11 04:22:07 +0000

    NuttX support has been updated for the new pre release of rtemlib.

--- nuttx/main.cpp
+++ nuttx/main.cpp
@@ -10,23 +10,18 @@
 #include <poll.h>
 #include <drivers/drv_hrt.h>
 
-//#include "Monitor_One.h"
-#include "Monitor_One.h"
+// sample monitor
+#include "monitor_set1.h"
+#include "task_compat.h"
 
 
-extern "C" __EXPORT int Monitor_main(int argc, char * const argv[]);
-
-static RTEML_buffer<int, 20> __buffer;
-static RTEML_writer<int> __writer;
-static RTEML_reader<int> __reader;
-
-Monitor_One mon_one(__buffer, 1000000); // period in microseconds
+extern "C" __EXPORT int rtemlib_main(int argc, char * const argv[]);
 
 static int daemon_task;
 
 static int monitor_main_loop(int argc, char **argv)
 {
-	bool state;
+	/*bool state;
 	__buffer.configWriter(__writer);
 	__buffer.configReader(__reader);
 	
@@ -49,17 +44,98 @@ static int monitor_main_loop(int argc, char **argv)
 
 	// begin the execution of the monitor
 	mon_one.enable();
+	*/
+
+	auto producer1 = [](void *) -> void*
+	{
+		auto enqueue_test = [](const int &data)
+		{
+
+		    // lets do one test
+		    irqstate_t flags;
+		    flags = irqsave();
+		    //__disable_irq();
+		    //sched_lock();
+		    static uint32_t local = 0;
+		    uint32_t * dest = &local;
+
+		    START_MEASURE();
+		    ATOMIC_begin(!, dest);
+		    COUNT_CYCLE();
+		    /*for(int i=0; i<10000; i++)
+		    {
+		        NOP;
+		    }*/
+		    //ISB;
+		    ATOMIC_end(dest);
+		    STOP_MEASURE();
+		    //sched_unlock();
+		    
+		    irqrestore(flags);
+		    //__enable_irq();
+
+		};
+
+		static RTEML_writer<int> __writer = RTEML_writer<int>(__buffer_monitor_set1.getBuffer());
+
+		enqueue_test(1);
+
+		START_MEASURE();
+		for(int i=0; i<10; i++)
+			__writer.enqueue(i);
+		STOP_MEASURE();
+
+		__buffer_monitor_set1.debug();
+		return NULL;
+	};
+
+	auto consumer1 = [](void *) -> void*
+	{
+		static RTEML_reader<int> __reader = RTEML_reader<int>(__buffer_monitor_set1.getBuffer());
+		Event<int> tmpEvent;
+
+		std::pair<state_rd_t,Event<int> > rd_tuple = __reader.dequeue();
+		tmpEvent = rd_tuple.second;
+		::printf("Event_consumed: %lu, %d code: %d\n", tmpEvent.getTime(), tmpEvent.getData(), rd_tuple.first);
+
+		// lets get a set of values instead of only one
+		auto rd_tuple_10 = __reader.dequeue_20();
+		uint32_t to_consume = std::get<1>(rd_tuple_10);
+		auto rd_tuple_array = std::get<2>(rd_tuple_10);
+		
+		uint32_t consumed = 0;
+
+		for (auto i=rd_tuple_array.begin(); i != rd_tuple_array.end(); i++)
+		{
+			if(consumed >= to_consume)
+				break;
+			
+			consumed++;
+
+			tmpEvent = *i;
+			::printf("Event_consumed_n: %lu, %d\n", tmpEvent.getTime(), tmpEvent.getData());
+		}
+
+		return NULL;
+	};
+
+	__attribute__ ((unused)) __task producer_1 = __task("producer1", producer1, 242, SCHED_FIFO, 1000000);
+
+	__attribute__ ((unused)) __task consumer_1 = __task("consumer1", consumer1, sched_get_priority_max(SCHED_FIFO), SCHED_FIFO, 2350000);
+
+
+	while(true) {sleep(1);}; // do sleep (delay)
 
 	return OK;
 }
 
-int Monitor_main(int argc, char * const argv[])
+int rtemlib_main(int argc, char * const argv[])
 {
 	::printf("Mon starting...\n");
 
-	daemon_task = px4_task_spawn_cmd("Monitor",
+	daemon_task = px4_task_spawn_cmd("rtemlib_main",
                                      SCHED_FIFO,
-                                     40,
+                                     241,
                                      2048,
                                      monitor_main_loop,
                                      NULL);
@@ -67,6 +143,8 @@ int Monitor_main(int argc, char * const argv[])
 
 	::printf("Mon started.\n");
 
-	return OK;
+	exit(0);
+
+	//return OK;
 }
 
--- nuttx/module.mk
+++ nuttx/module.mk
@@ -1,8 +1,12 @@
 #
 # rtemlib module for NuttX OS
-# 
+#
+
+# editable settings...
+MONITOR_SOURCE_FILES = ../synth_tool/monitor_set1/monitor_set1.cpp
+MONITOR_FOLDER = $(SKETCHBOOK)/modules/rtemlib/synth_tool/monitor_set1
 
 MODULE_COMMAND = rtemlib
-SRCS = main.cpp
+SRCS = libatomic.c main.cpp ../RTEML_monitor.cpp $(MONITOR_SOURCE_FILES)
 MODULE_STACKSIZE = 4096
-EXTRACXXFLAGS = -Wframe-larger-than=1200 -DCONFIG_WCHAR_BUILTIN -I$(SKETCHBOOK)/../rteml/arch/arm/include -DARM_CM4_FP -D__NUTTX__ -std=c++0x --verbose
+EXTRACXXFLAGS = -Wframe-larger-than=1200 -DCONFIG_WCHAR_BUILTIN -I$(SKETCHBOOK)/modules/rtemlib -I$(SKETCHBOOK)/modules/rtemlib/arch/arm/include -I$(MONITOR_FOLDER) -DARM_CM4_FP -D__NUTTX__ -DDEBUG=3 -std=c++0x --verbose -w
