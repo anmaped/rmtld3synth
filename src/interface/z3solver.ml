c3054c0e2d1f310b1fcc1a3ade779c63ae072390
André Pedro <apedro.1@gmail.com>
2023-10-21 18:53:14 +0100

    Fix z3 store array.

--- src/interface/z3solver.ml
+++ src/interface/z3solver.ml
@@ -1,9 +1,6 @@
-
 (*
-   Interface for Z3 smt solver
+   Interface for Z3 SMT solver (<= 4.7.1)
  *)
-
-open Batteries
 
 open Z3
 open Z3.SMT
@@ -17,71 +14,107 @@ open Z3enums
 open Z3.Symbol
 open Z3.Arithmetic
 open Z3.Tactic
-
 open Helper
 
 let parse_smtlibv2 smtlibv2_str =
   (*ignore (Z3.Log.open_ "Z3.log");*)
-  verb (fun _ -> Printf.printf "Running Z3 version %s\n" Version.to_string ) ;
-  verb (fun _ -> Printf.printf "Z3 full version string: %s\n" Version.full_version ) ;
-  let ctx = (mk_context [("model", "true")]) in
+  verb (fun _ -> Printf.printf "Running Z3 version %s\n" Version.to_string);
+  verb (fun _ ->
+      Printf.printf "Z3 full version string: %s\n" Version.full_version);
+  let ctx = mk_context [ ("model", "true") ] in
   let exp = parse_smtlib2_string ctx smtlibv2_str [] [] [] [] in
-  verb (fun _ -> print_endline "\nProblem:\n" ; print_endline (Z3.Expr.to_string exp) ) ;
-  (ctx, [exp])
+  verb (fun _ ->
+      print_endline "\nProblem:\n";
+      print_endline (Z3.Expr.to_string exp));
+  (ctx, [ exp ])
 
 let solve_ ctx exp_lst =
-  let tactic_set = and_then ctx (mk_tactic ctx "qe") (mk_tactic ctx "smt") []
-  in let solver = mk_solver_t ctx tactic_set
-  in let _ = add solver exp_lst
-  in let sol = (string_of_status (check solver []), solver)
-  in verb (fun _ -> print_endline (Statistics.to_string (get_statistics solver) ) ) ;
+  let tactic_set = and_then ctx (mk_tactic ctx "qe") (mk_tactic ctx "smt") [] in
+  let solver = mk_solver_t ctx tactic_set in
+  let _ = add solver exp_lst in
+  let sol = (string_of_status (check solver []), solver) in
+  verb (fun _ -> print_endline (Statistics.to_string (get_statistics solver)));
   sol
 
-
 let get_model ctx solver =
-  
-  let model = match get_model solver with Some x -> x | None -> raise (Failure ("Model is not available.")) in
+  let model =
+    match get_model solver with
+    | Some x -> x
+    | None -> raise (Failure "Model is not available.")
+  in
 
-  verb (fun _ -> print_endline "\nModel:\n" ) ;
-  verb (fun _ -> print_endline (Z3.Model.to_string model) ) ;
+  verb (fun _ -> print_endline "\nModel:\n");
+  verb (fun _ -> print_endline (Z3.Model.to_string model));
   model
 
-let string_of_z3model model =
-  Z3.Model.to_string model
-
+let string_of_z3model model = Z3.Model.to_string model
 
 let get_scheduler ctx model helper =
-  verb (fun _ -> List.iter (fun el -> print_endline ( "kind: "^(string_of_int (int_of_decl_kind (get_decl_kind el) ) )^ " Name: " ^ ( get_string (get_name el) ) ) )  (get_decls model) ) ;
-  
-  let trace = List.find (fun el -> get_string (get_name el) = "trc") (get_decls model) in
-  let trace_size = List.find (fun el -> get_string (get_name el) = "trc_size") (get_decls model) in
+  verb (fun _ ->
+      List.iter
+        (fun el ->
+          print_endline
+            ("kind: "
+            ^ string_of_int (int_of_decl_kind (get_decl_kind el))
+            ^ " Name: "
+            ^ get_string (get_name el)))
+        (get_decls model));
 
-  let interp = (match get_func_interp model trace with Some x -> x | None -> raise (Failure ("Function interpret is not available."))) in
-  verb (fun _ -> print_endline ("N interp: "^(string_of_int (get_num_entries interp)) ) ) ;
-  verb (fun _ -> print_endline (Z3.Model.FuncInterp.to_string interp) ) ;
+  let trace =
+    List.find (fun el -> get_string (get_name el) = "trc") (get_decls model)
+  in
+  let trace_size =
+    List.find
+      (fun el -> get_string (get_name el) = "trc_size")
+      (get_decls model)
+  in
+
+  let interp =
+    match get_func_interp model trace with
+    | Some x -> x
+    | None -> raise (Failure "Function interpret is not available.")
+  in
+  verb (fun _ ->
+      print_endline ("N interp: " ^ string_of_int (get_num_entries interp)));
+  verb (fun _ -> print_endline (Z3.Model.FuncInterp.to_string interp));
 
   let size_fun_interp =
-  (
     match get_const_interp model trace_size with
     | Some x -> x
-    | None -> raise (Failure ("Interpretation of the function (size_fun_interp) is not available."))
-  ) in
+    | None ->
+        raise
+          (Failure
+             "Interpretation of the function (size_fun_interp) is not \
+              available.")
+  in
 
-  verb (fun _ -> print_endline ("\nTrace size: "^(Z3.Expr.to_string size_fun_interp)^"\n") ) ;
+  verb (fun _ ->
+      print_endline ("\nTrace size: " ^ Z3.Expr.to_string size_fun_interp ^ "\n"));
 
   (* set default size to 2 when trace_size is unknown/equal to (- 1); sum two to the trace_size value otherwise *)
-  let size = if (Z3.Expr.to_string size_fun_interp) = "(- 1)" then 2 else int_of_string (Z3.Expr.to_string size_fun_interp) + 2 in
+  let size =
+    if Z3.Expr.to_string size_fun_interp = "(- 1)" then 2
+    else int_of_string (Z3.Expr.to_string size_fun_interp) + 2
+  in
 
   let exp2 = get_else interp in
 
   (* replace variables and evaluate *)
-  List.fold_right (fun a b ->
-    let rpl_exp = Expr.substitute_vars exp2 [Integer.mk_numeral_i ctx a] in
-    let evaluated_expression = (match eval model rpl_exp true with Some x -> x | None -> raise (Failure ("Evaluation is not available."))) in
-    let exp_value = (Z3.AST.to_string (ast_of_expr evaluated_expression)) in
-    verb (fun _ -> print_endline ("NM: "^exp_value) ; ) ;
-    verb (fun _ -> print_endline (string_of_int a^" -> "^ (find_proposition_rev_hashtbl (int_of_string exp_value)  helper)) ) ;
-    (find_proposition_rev_hashtbl (int_of_string exp_value)  helper)::b
-    (* print_endline (Z3.AST.to_string (ast_of_expr rpl_exp)) ; *)
-  ) (List.of_enum (0--size)) []
-  
+  List.fold_right
+    (fun a b ->
+      let rpl_exp = Expr.substitute_vars exp2 [ Integer.mk_numeral_i ctx a ] in
+      let evaluated_expression =
+        match eval model rpl_exp true with
+        | Some x -> x
+        | None -> raise (Failure "Evaluation is not available.")
+      in
+      let exp_value = Z3.AST.to_string (ast_of_expr evaluated_expression) in
+      verb (fun _ -> print_endline ("NM: " ^ exp_value));
+      verb (fun _ ->
+          print_endline
+            (string_of_int a ^ " -> "
+            ^ find_proposition_rev_hashtbl (int_of_string exp_value) helper));
+      find_proposition_rev_hashtbl (int_of_string exp_value) helper :: b
+      (* print_endline (Z3.AST.to_string (ast_of_expr rpl_exp)) ; *))
+    (List.of_enum (0 -- size))
+    []
--- src/interface/z3solver_newAPI.ml
+++ src/interface/z3solver_newAPI.ml
@@ -1,4 +1,6 @@
-(* Interface for Z3 smt solver *)
+(*
+   Interface for Z3 SMT solver (> 4.7.1)
+ *)
 
 open Z3
 open Z3.SMT
@@ -16,24 +18,22 @@ open Helper
 
 let parse_smtlibv2 smtlibv2_str =
   (* ignore (Z3.Log.open_ "Z3.log"); *)
-  verb (fun _ -> Printf.printf "Running Z3 version %s\n" Version.to_string) ;
+  verb (fun _ -> Printf.printf "Running Z3 version %s\n" Version.to_string);
   verb (fun _ ->
-      Printf.printf "Z3 full version string: %s\n" Version.full_version ) ;
-  let ctx = mk_context [("model", "true")] in
+      Printf.printf "Z3 full version string: %s\n" Version.full_version);
+  let ctx = mk_context [ ("model", "true") ] in
   let exp = parse_smtlib2_string ctx smtlibv2_str [] [] [] [] in
   verb (fun _ ->
-      print_endline "\nProblem:\n" ;
-      print_endline (Z3.AST.ASTVector.to_string exp) ) ;
+      print_endline "\nProblem:\n";
+      print_endline (Z3.AST.ASTVector.to_string exp));
   (ctx, Z3.AST.ASTVector.to_expr_list exp)
 
 let solve_ ctx exp =
-  let tactic_set =
-    and_then ctx (mk_tactic ctx "qe") (mk_tactic ctx "smt") []
-  in
+  let tactic_set = and_then ctx (mk_tactic ctx "qe") (mk_tactic ctx "smt") [] in
   let solver = mk_solver_t ctx tactic_set in
   let _ = add solver exp in
   let sol = (string_of_status (check solver []), solver) in
-  verb (fun _ -> print_endline (Statistics.to_string (get_statistics solver))) ;
+  verb (fun _ -> print_endline (Statistics.to_string (get_statistics solver)));
   sol
 
 let get_model ctx solver =
@@ -42,23 +42,23 @@ let get_model ctx solver =
     | Some x -> x
     | None -> raise (Failure "Model is not available.")
   in
-  verb (fun _ -> print_endline "\nModel:\n") ;
-  verb (fun _ -> print_endline (Z3.Model.to_string model)) ;
+  verb (fun _ -> print_endline "\nModel:\n");
+  verb (fun _ -> print_endline (Z3.Model.to_string model));
   model
 
 let string_of_z3model model = Z3.Model.to_string model
 
 let get_scheduler ctx model helper =
-  verb (fun _ -> print_endline "get_scheduler") ;
+  verb (fun _ -> print_endline "get_scheduler");
   verb (fun _ ->
       List.iter
         (fun el ->
           print_endline
-            ( "kind: "
+            ("kind: "
             ^ string_of_int (int_of_decl_kind (get_decl_kind el))
             ^ " Name: "
-            ^ get_string (get_name el) ) )
-        (get_decls model) ) ;
+            ^ get_string (get_name el)))
+        (get_decls model));
   let seq_size =
     List.find
       (fun el -> get_string (get_name el) = "trc_size")
@@ -74,7 +74,7 @@ let get_scheduler ctx model helper =
   in
   verb (fun _ ->
       print_endline
-        ("\nSequence size: " ^ Z3.Expr.to_string size_fun_interp ^ "\n") ) ;
+        ("\nSequence size: " ^ Z3.Expr.to_string size_fun_interp ^ "\n"));
   let size = int_of_string (Z3.Expr.to_string size_fun_interp) + 2 in
   (* retrieve time stamps *)
   let timed_seq_model_exp =
@@ -86,7 +86,7 @@ let get_scheduler ctx model helper =
       in
       let exp_timed_seq = Z3.FuncDecl.apply timed_seq [] in
       verb (fun _ ->
-          print_endline (Z3.AST.to_string (ast_of_expr exp_timed_seq)) ) ;
+          print_endline (Z3.AST.to_string (ast_of_expr exp_timed_seq)));
       let timed_seq_model_exp =
         match eval model exp_timed_seq true with
         | Some x -> x
@@ -95,8 +95,8 @@ let get_scheduler ctx model helper =
       in
       verb (fun _ ->
           print_endline
-            ( "Timed Sequence: "
-            ^ Z3.AST.to_string (ast_of_expr timed_seq_model_exp) ) ) ;
+            ("Timed Sequence: "
+            ^ Z3.AST.to_string (ast_of_expr timed_seq_model_exp)));
       Some timed_seq_model_exp
     with _ -> None
   in
@@ -104,64 +104,67 @@ let get_scheduler ctx model helper =
   let seq_model =
     try
       let seq =
-        List.find
-          (fun el -> get_string (get_name el) = "trc")
-          (get_decls model)
+        List.find (fun el -> get_string (get_name el) = "trc") (get_decls model)
       in
       let exp_seq = Z3.FuncDecl.apply seq [] in
-      verb (fun _ -> print_endline (Z3.AST.to_string (ast_of_expr exp_seq))) ;
+      verb (fun _ -> print_endline (Z3.AST.to_string (ast_of_expr exp_seq)));
       let seq_model =
         match eval model exp_seq true with
         | Some x -> x
         | None -> raise (Failure "Evaluation of exp_seq is not available.")
       in
       verb (fun _ ->
-          print_endline
-            ("Sequence: " ^ Z3.AST.to_string (ast_of_expr seq_model)) ) ;
+          print_endline ("Sequence: " ^ Z3.AST.to_string (ast_of_expr seq_model)));
       seq_model
     with _ -> failwith "No sequence available."
   in
   let get_array_interp_from_exp exp i : string =
-    (* check if array is a quantifier/lambda or a constant array *)
-    if not (Z3.AST.is_quantifier (ast_of_expr exp)) then
-      if not (Z3.Z3Array.is_constant_array exp) then
-        failwith
-          ( "trc is not a quantifier/lambda nor a constant array: '"
-          ^ Z3.AST.to_string (ast_of_expr exp)
-          ^ " '"
-          ^ string_of_int
-              (Z3enums.int_of_ast_kind (Z3.AST.get_ast_kind (ast_of_expr exp)))
-          ^ "'" ) ;
-    if Z3.Z3Array.is_constant_array exp then
-      Z3.AST.to_string (ast_of_expr (List.hd (Z3.Expr.get_args exp)))
-    else
+    (*
+        check if array is a quantifier/lambda, a constant array, or a store array;
+        error otherwise
+    *)
+    if Z3.Z3Array.is_constant_array exp || Z3.Z3Array.is_store exp then
+      let exp = Z3.Z3Array.mk_select ctx exp (Integer.mk_numeral_i ctx i) in
+      let evaluated_expression =
+        match eval model exp true with
+        | Some x -> x
+        | None -> raise (Failure "Evaluation is not available.")
+      in
+      Z3.AST.to_string (ast_of_expr evaluated_expression)
+    else if Z3.AST.is_quantifier (ast_of_expr exp) then
       let body =
         Z3.Quantifier.get_body (Z3.Quantifier.quantifier_of_expr exp)
       in
-      let rpl_exp = Expr.substitute_vars body [Integer.mk_numeral_i ctx i] in
+      let rpl_exp = Expr.substitute_vars body [ Integer.mk_numeral_i ctx i ] in
       let evaluated_expression =
         match eval model rpl_exp true with
         | Some x -> x
         | None -> raise (Failure "Evaluation is not available.")
       in
       Z3.AST.to_string (ast_of_expr evaluated_expression)
+    else
+      failwith
+        ("trc is NOT a quantifier/lambda, a constant array, or a store array: '"
+        ^ Z3.AST.to_string (ast_of_expr exp)
+        ^ "' "
+        ^ string_of_int
+            (Z3enums.int_of_ast_kind (Z3.AST.get_ast_kind (ast_of_expr exp)))
+        ^ " ")
   in
   (* replace variables and evaluate terms *)
   List.fold_right
     (fun a b ->
-      ( match timed_seq_model_exp with
+      (match timed_seq_model_exp with
       | Some v' ->
           let t' = get_array_interp_from_exp v' a in
-          verb (fun _ -> print_endline ("t': " ^ t')) ;
+          verb (fun _ -> print_endline ("t': " ^ t'));
           ()
-      | None -> () ) ;
+      | None -> ());
       let exp_value = get_array_interp_from_exp seq_model a in
-      verb (fun _ -> print_endline ("value: " ^ exp_value)) ;
+      verb (fun _ -> print_endline ("value: " ^ exp_value));
       verb (fun _ ->
           print_endline
-            ( string_of_int a ^ " -> "
-            ^ find_proposition_rev_hashtbl (int_of_string exp_value) helper )
-      ) ;
-      find_proposition_rev_hashtbl (int_of_string exp_value) helper :: b )
-    (of_enum 0 size)
-    []
+            (string_of_int a ^ " -> "
+            ^ find_proposition_rev_hashtbl (int_of_string exp_value) helper));
+      find_proposition_rev_hashtbl (int_of_string exp_value) helper :: b)
+    (of_enum 0 size) []
